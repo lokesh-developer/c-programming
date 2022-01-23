d73fc8bc25277c45555a093275d18679c9d07c0c
Lokesh Kabra <64826892+lokesh-developer@users.noreply.github.com>
2022-01-24 03:16:13 +0530

    Create expb.c

---
+++ expb.c
@@ -0,0 +1,21 @@
+// write the program for addition of two numbers
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int a, b, c;
+    clrscr();
+    printf("Enter the value of a & b");
+    scanf("%d%d", &a, &b);
+    c = a + b;
+    printf("The addition of two numbers is %d\n", c);
+    c = a - b;
+    printf("The subtraction of two numbers is %d\n", c);
+    c = a * b;
+    printf("The product of two numbers is %d\n", c);
+    c = a / b;
+    printf("The division of two numbers is %d\n", c);
+    getch();
+}
