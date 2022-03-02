8db310ad44cbe1044fe0e50318f12e72e82c2aac
Lokesh Kabra <kabral1999@gmail.com>
2022-03-02 22:25:48 +0530

    some more concepts :)

---
+++ forloop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main()
+{
+    int i = 11;
+    // = 3 == true false
+    if (i < 10)
+    {
+        // Nested if else inside the body of "if"
+        if (i < 5)
+        {
+            // Statements inside the body of nested "if"
+        }
+        else
+        {
+            // Statements inside the body of nested "else"
+        }
+    }
+    else
+    {
+        // Statements inside the body of "else"
+    }
+}
